bff5eb21a76f567a859f1533c57692c067a7ae24
Adem Layes <ademlayesjr@gmail.com>
2023-06-20 15:16:33 +0100

    finished git part in the video

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
 function myFun() {
-  document.getElementById('para').innerHTML = 'changes'
+  let para = document.getElementById('para');
+  let paraValue = para.innerHTML
+  if(paraValue == "this is lovely!") {
+    para.innerHTML = "Changed"
+  }else{
+    para.innerHTML = "this is lovely!"
+  }
 }
-// this is just a comment
+/*
+
+Git commends i learned so far:
+
+- git config --global user.name 'name'
+  git config --global user.email 'email'
+- git init
+  git init 'repo name'
+- git status
+- git log
+- git touch 'file name'
+- git cd 'derectory'
+- ls
+  ls -a
+- git branch
+  git branch 'branch name'
+- git checkout 'branch name or master'
+- git add 'file name'
+  git add .
+  git add *.html
+- git commit -m "message"
+- git rm --cached 'file name'
+- git merge 'branch name'
+*/
