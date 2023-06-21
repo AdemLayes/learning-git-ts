cdd2565aec6dbb7a8b452b96edddfc13bf8f716f
Adem Layes <ademlayesjr@gmail.com>
2023-06-21 13:33:34 +0100

    Add git commends text file

--- index.js
+++ index.js
@@ -7,33 +7,3 @@ function myFun() {
     para.innerHTML = "this is lovely!"
   }
 }
-/*
-
-Git commends i learned so far:
-
-- git config --global user.name 'name'
-  git config --global user.email 'email'
-- git init
-  git init 'repo name'
-- git status
-- git log
-- git touch 'file name'
-- git cd 'derectory'
-- ls
-  ls -a
-- git branch
-  git branch 'branch name'
-- git checkout 'branch name or master'
-- git add 'file name'
-  git add .
-  git add *.html
-- git commit -m "message"
-- git rm --cached 'file name'
-- git merge 'branch name'
-- git remote -v
-  git remote add 'remote branch name' 'url(from gitHub)'
-- git push -u 'remote branch name' 'branch name / master name'
-- git pull 'remote branch name' 'branch name / master name'
-*/
-// this is just a comment
-// boga boga baby!
